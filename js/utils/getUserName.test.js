210a10f30f5a936ef01c55edfe89558659ed817c
johnruud3 <johnruud@hotmail.no>
2025-10-17 14:55:28 +0200

    - Installed prettier and ESlinter - Added tests to "isActivePath" function to validate if the return is correct with vitest. - Installed /* @vitest-environment jsdom */ to getUsername.test. To be able to add a browser-like api to be able to test with localStorage.

---
+++ js/utils/getUserName.test.js
@@ -0,0 +1,15 @@
+/* @vitest-environment jsdom */
+import { describe, it, expect } from "vitest";
+import { saveUser, getUsername, clearStorage } from "./storage.js";
+
+describe("getUsername", () => {
+    it("should return the username from storage", () => {
+        clearStorage();
+        saveUser({ name: "John" });
+        expect(getUsername()).toBe("John");
+    });
+    it("should return null if no user exists in storage", () => {
+        clearStorage();
+        expect(getUsername()).toBe(null);
+    });
+});
---
+++ js/utils/isActivePath.test.js
@@ -0,0 +1,26 @@
+import { describe, it, expect } from "vitest";
+import { isActivePath } from "./userInterface.js";
+
+
+// If true the link should be active and get the correct styling
+describe("isActivePath", () => {
+  it("should return true if the path is the same as the current path", () => {
+    expect(isActivePath("/", "/")).toBe(true);
+  });
+
+  it("should return true when current path matches href exactly (non-root)", () => {
+    expect(isActivePath("/register", "/register")).toBe(true);
+  });
+
+  it("should return true for root path '/' when current path is '/index.html'", () => {
+    expect(isActivePath("/", "/index.html")).toBe(true);
+  });
+
+  it("should return true when current path includes the href", () => {
+    expect(isActivePath("/venue", "/venue/123")).toBe(true);
+  });
+
+  it("should return false when paths don't match", () => {
+    expect(isActivePath("/login", "/register")).toBe(false);
+  });
+});
